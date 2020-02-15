2278e32893217f497de0a5753695ea2eab0096f9
AlessandroRodrigo <alessandro.perez@etec.sp.gov.br>
2020-02-15 20:18:43 -0300

    Desenvolvimento da parte web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack:pedepano123@cluster0-kxfbp.mongodb.net/week10?retryWrites=true&w=majority', {
-	useUnifiedTopology: true,
-	useNewUrlParser: true
-});
+mongoose.connect(
+  'mongodb+srv://omnistack:pedepano123@cluster0-kxfbp.mongodb.net/week10?retryWrites=true&w=majority',
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  }
+);
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
